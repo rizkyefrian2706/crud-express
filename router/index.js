d7cc14a19fcf051202add53a64dace266c6df6e4
rizkyefrian2706 <rizkyefrian2706@gmail.com>
2022-09-16 16:59:34 +0700

    a

--- router/index.js
+++ router/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const user = require('../controller/user')
-
-router.get('/logout', auth.logout)
+ 
 router.get('/user', user.index)  
 router.get('/user/:id', user.show)  
 router.post('/user', user.create)  
